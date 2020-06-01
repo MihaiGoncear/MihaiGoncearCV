1605c19a0c632ee72c61f2ad7108442cfa1a3161
MihaiGoncear <MihaiGoncear@users.noreply.github.com>
2020-06-01 17:26:47 +0300

    edit total

--- JS/mainEn.js
+++ JS/mainEn.js
@@ -1,56 +1,114 @@
+// Script to style the navigation-skills section
+
 let education = document.getElementById('nav-education');
 let training = document.getElementById('nav-training');
 let experience = document.getElementById('nav-experience');
+let homeBtn = document.getElementById('nav-home');
+let personalInfoButt = document.getElementById('nav-personal-info')
 
-let title = document.getElementById('section2-default')
+let home = document.getElementById('home')
 let dl1 = document.getElementById('dl1');
 let dl2 = document.getElementById('dl2');
 let dl3 = document.getElementById('dl3');
+let personalInfo = document.getElementById('personal-info');
+
 
 education.onclick = revealEducation;
-
-function revealEducation() {
-    dl1.classList.remove('hidden');
-    education.style.backgroundColor = "rgb(31, 31, 160)"
-    education.style.color = "rgb(234, 234, 255)"
-    training.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    training.style.color = "black"
-    experience.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    experience.style.color = "black"
-    title.classList.remove('section2-default')
-    title.classList.add('hidden');
-    dl2.classList.add('hidden')
-    dl3.classList.add('hidden')
-}
-
 training.onclick = revealTraining;
-
-function revealTraining() {
-    dl2.classList.remove('hidden');
-    training.style.backgroundColor = "rgb(31, 31, 160)"
-    training.style.color = "rgb(234, 234, 255)"
-    experience.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    experience.style.color = "black"
-    education.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    education.style.color = "black"
-    title.classList.remove('section2-default')
-    title.classList.add('hidden');
-    dl1.classList.add('hidden')
-    dl3.classList.add('hidden')
-}
-
 experience.onclick = revealExperience;
 
+function revealEducation() {
+    contentReveal(dl1, dl2, dl3, education, training, experience, homeBtn, home, personalInfo, personalInfoButt)
+}
+
+function revealTraining() {
+    contentReveal(dl2, dl1, dl3, training, experience, education, homeBtn, home, personalInfo, personalInfoButt)
+}
+
 function revealExperience() {
-    dl3.classList.remove('hidden');
-    experience.style.backgroundColor = "rgb(31, 31, 160)"
-    experience.style.color = "rgb(234, 234, 255)"
-    education.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    education.style.color = "black"
-    training.style.backgroundColor = "rgba(166, 166, 223, 0.801)"
-    training.style.color = "black"
-    title.classList.remove('section2-default')
-    title.classList.add('hidden');
-    dl2.classList.add('hidden')
-    dl1.classList.add('hidden')
+    contentReveal(dl3, dl1, dl2, experience, education, training, homeBtn, home, personalInfo, personalInfoButt)
+}
+
+function contentReveal(firstSect, secondSect, thirdSect, firstButt, secondButt, thirdButt, homeBtn, home, personalInfo, personalInfoButt){
+    firstSect.classList.remove('hidden');
+    secondSect.classList.add('hidden');
+    thirdSect.classList.add('hidden');
+
+    firstButt.style.backgroundColor = "rgb(68, 68, 161)";
+    firstButt.style.color = "rgb(234, 234, 255)";
+    firstButt.style.height = "50px"
+
+    secondButt.style.backgroundColor = "";
+    secondButt.style.color = "";
+    secondButt.style.height = "";
+    secondButt.classList.add('navigation-skills-style');
+    
+
+    thirdButt.style.backgroundColor = "";
+    thirdButt.style.color = "";
+    thirdButt.style.height = "";
+    thirdButt.classList.add('navigation-skills-style');
+
+    homeBtn.style.backgroundColor = "";
+    homeBtn.style.color = "";
+    homeBtn.style.height = "";
+    homeBtn.classList.add('main-navigation-style');
+
+    personalInfoButt.style.backgroundColor = "";
+    personalInfoButt.style.color = "";
+    personalInfoButt.style.height = "";
+    personalInfoButt.classList.add('main-navigation-style');    
+
+    home.classList.remove('home');
+    home.classList.add('hidden');
+
+    personalInfo.classList.remove('test');
+    personalInfo.classList.add('hidden');
+
+
+}
+
+// Script to style the main-navigation section
+
+homeBtn.onclick = revealHome;
+personalInfoButt.onclick = revealPersonalInfo
+function revealHome() {
+    mainReveal(dl1, dl2, dl3, education, training, experience, homeBtn, personalInfoButt, home, personalInfo)
+}
+function revealPersonalInfo() {
+    mainReveal(dl1, dl2, dl3, education, training, experience, personalInfoButt, homeBtn, personalInfo, home)
+}
+
+function mainReveal(dl1, dl2, dl3, education, training, experience, firstButt,secondButt, firstSect, secondSect ){
+    dl1.classList.add('hidden');
+    dl2.classList.add('hidden');
+    dl3.classList.add('hidden');
+    
+    education.style.backgroundColor = "";
+    education.style.color = "";
+    education.style.height = "";
+    education.classList.add('navigation-skills-style');
+
+    training.style.backgroundColor = "";
+    training.style.color = "";
+    training.style.height = "";
+    training.classList.add('navigation-skills-style');
+    
+    experience.style.backgroundColor = "";
+    experience.style.color = "";
+    experience.style.height = "";
+    experience.classList.add('navigation-skills-style');
+
+    firstButt.style.backgroundColor = "rgb(161, 68, 68)";
+    firstButt.style.color = "rgb(234, 234, 255)";
+    firstButt.style.height = "50px";
+
+    secondButt.style.backgroundColor = "";
+    secondButt.style.color = "";
+    secondButt.style.height = "";
+    secondButt.classList.add('main-navigation-style');
+
+    firstSect.classList.remove('hidden')
+    secondSect.classList.remove('home')
+    secondSect.classList.add('hidden')
 }
